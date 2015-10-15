cedeaee5ee06c2a10ddfc34710e7470c12e2d711
Cort Harlow <cort.harlow@gmail.com>
2015-10-15 16:07:17 -0400

    Added ingredient click events, starting checkWin

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 window.onload = function() {
+
   var BurritoChallenge = (function() {
+
+  /////////////////////Declare global variables///////////////////
 
     //17 ingredients
     var ingredientsArr = ['White Rice', 'Brown Rice', 'Pinto Beans', 'Black Beans', 'Barbacoa', 'Carnitas', 'Chicken', 'Steak', 'Tomatoes', 'Corn', 'Green Salsa', 'Red Salsa', 'Sour Cream', 'Cheese', 'Fajitas', 'Lettuce', 'Guacamole'];
-    var wins = 3;
+    var currentTime = Number($('#timer').text());
+    var wins = 0;
+    var checkOrder = [];
+    var timerHandle;
+
+//////////////////////////Private Functions///////////////////////
 
     var timerCountdown = function() {
       timerHandle = setInterval(function () {
-        var currentTime = Number($('#timer').text());
         currentTime--;
         $('#timer').text(currentTime);
+        if (currentTime === 0) {
+          clearInterval(timerHandle);
+        }
       }, 1000);
     }
 
@@ -22,53 +32,95 @@ window.onload = function() {
       $('body').append(startover);
     }
 
-    var randomNum = function() {
-      var num = Math.floor(Math.random() * (16 - 0 + 1)) + 0;
+    var randomNum = function(max) {
+      var num = Math.floor(Math.random() * (max - 0 + 1)) + 0;
       return num;
     }
 
     var orderAlert = function() {
       var alert = 'New Order!';
       if (wins > 6) {
-        alert += '\n'+getOrder(9);
-      }
-
-      else if (wins > 4) {
         alert += '\n'+getOrder(7);
       }
-
-      else if (wins > 2) {
+      else if (wins > 4) {
         alert += '\n'+getOrder(5);
       }
-
+      else if (wins > 2) {
+        alert += '\n'+getOrder(4);
+      }
       else {
         alert += '\n'+getOrder(3);
       }
       window.alert(alert);
+      console.log(alert);
     }
 
     var getOrder = function (numIng) {
+      var currentArr = ingredientsArr;
+      var max = 16
       var order = '';
       for (var i = 0; i <= numIng; i++) {
-        order += '\n'+ingredientsArr[randomNum()];
+        var num = randomNum(max);
+        order += '\n'+currentArr[num];
+        checkOrder.push(currentArr[num]);
+        currentArr.splice(num, 1);
+        max--;
       }
       return order;
     }
 
+    var finishButton = function() {
+      $('body').on('click', '#finish', (function() {
+        clearInterval(timerHandle);
+        console.log(checkOrder);
+        var num = checkOrder.length;
+        var burritoArr = [];
+        for (var i = 0; i < num; i++) {
+          burritoArr.push($('p.ingtext').eq(i).text());
+        }
+        console.log(burritoArr);
+      }));
+    }
+
+    var startOver = function() {
+      $('body').on('click', '#startover', (function() {
+        location.reload(true);
+      }));
+    }
+
+/////////////////////////PUBLIC FUNCTIONS/////////////////////////
+
     return {
 
       buttonStart : function() {
-        $('button#start').click(function () {
+        $('button#start').click(function() {
           timerCountdown();
           addButtons();
           orderAlert();
         })
       },
 
+      addIngredientClicks : function() {
+        $('#ingredients-list').on('click', '.click', (function() {
+          $(this).attr('class', 'inside');
+          $(this).css('border', '4px solid black').css('border-radius', '5px').css('margin',
+        '1px 0px 1px 3px');
+          $('#burrito').append($(this));
+        }))
+      },
 
+      finishButton : function() {
+        finishButton();
+      },
 
-
+      startoverButton : function() {
+        startOver();
+      }
+      
     }
 })();
   BurritoChallenge.buttonStart();
+  BurritoChallenge.addIngredientClicks();
+  BurritoChallenge.finishButton();
+  BurritoChallenge.startoverButton();
 }
