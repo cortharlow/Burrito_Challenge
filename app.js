991ac3d8cf67a1ff538a32faefbe0fa39838ce38
Cort Harlow <cort.harlow@gmail.com>
2015-10-15 11:53:47 -0400

    Full commit, adding alert functionality

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ window.onload = function() {
 
   var BurritoChallenge = (function() {
 
+    //17 ingredients
     var ingredientsArr = ['White Rice', 'Brown Rice', 'Pinto Beans', 'Black Beans', 'Barbacoa', 'Carnitas', 'Chicken', 'Steak', 'Tomatoes', 'Corn', 'Green Salsa', 'Red Salsa', 'Sour Cream', 'Cheese', 'Fajitas', 'Lettuce', 'Guacamole'];
+    var wins = 0;
 
     var timerCountdown = function() {
       timerHandle = setInterval(function () {
@@ -22,12 +24,42 @@ window.onload = function() {
       $('body').append(startover);
     }
 
+    var randomNum = function() {
+      var num = Math.floor(Math.random() * (16 - 0 + 1)) + 0;
+      return num;
+    }
+
+    var orderAlert = function() {
+      var alert = 'New Order!\n';
+      var numIng = 0;
+      if (wins < 2) {
+        numIng = 5;
+        for (var i = 0; i <= numIng; i++) {
+          alert += '\n'+ingredientsArr[num];
+        }
+      }
+
+      else if (wins > 2) {
+
+      }
+
+      else if (wins > 4) {
+
+      }
+
+      else if (wins > 6) {
+
+      }
+      alert(alert);
+    }
+
     return {
 
       buttonStart : function() {
         $('button#start').click(function () {
           timerCountdown();
           addButtons();
+          orderAlert();
 
 
         })
